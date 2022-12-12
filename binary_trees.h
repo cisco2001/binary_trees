4dbb29ac6d6afd68b64f542edf7a215e25224939
Mtwa Johakim Mgimwa <fransisco2001johakim@gmail.com>
2022-12-12 23:08:37 +0300

    add prototype for a function to visualize binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -10,15 +12,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif
